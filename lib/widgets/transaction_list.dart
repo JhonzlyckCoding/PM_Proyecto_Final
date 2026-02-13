fec0d58e6560d6eecf82c72f4d0fa75dc2cfd147
Daniel-Bonilla-190905 <23030200044@uppenjamo.edu.mx>
2026-02-13 08:36:57 -0600

    Lista de transacciones en procesos

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty 
+    ? Column(
+      children: <Widget> [
+        Text('No hay gastos todavia', style: Theme.of(context).textTheme.titleMedium), 
+        SizedBox(height: 20),
+      ],
+    )
+   : ListView.builder(
+       itemBuilder: (ctx, index){
+        return Card(
+          elevation: 5,
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+          color: Colors.deepPurple[50],
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: Padding(
+                padding: EdgeInsets.all(6),
+                child: FittedBox(child: Text('\$${transactions[index].amount}'),),
+                ),
+            ),
+            title: Text(
+              transactions
+            ),
+          ),
+
+        )
+       },
+   )
+  }
+}
